2670b90be7bdf87025a76d54207561f421ddae55
Kathy-Solano <katisv90@hotmail.com>
2014-06-14 14:28:58 -0600

    Se agrego la logica de tipo telefono

---
+++ SiprolimarApi/DAL/ITipoTelefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiprolimarDB.Entities;
+
+namespace SiprolimarApi.DAL
+{
+   public interface ITipoTelefono
+    {
+       List<TipoTelefono> getTipoTelefonos();
+       TipoTelefono getTipoTelefono(int id);
+    }
+}
